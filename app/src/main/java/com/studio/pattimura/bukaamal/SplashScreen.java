dc5cf40092795f0cde19f568b4a285b32f4f64aa
wildan <muhammadwildaniskandar@gmail.com>
2017-05-06 07:37:33 +0700

    update seadanya

--- app/src/main/java/com/studio/pattimura/bukaamal/SplashScreen.java
+++ app/src/main/java/com/studio/pattimura/bukaamal/SplashScreen.java
@@ -1,11 +1,15 @@
 package com.studio.pattimura.bukaamal;
 
 import android.content.Intent;
+import android.media.ImageReader;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 import com.wang.avi.AVLoadingIndicatorView;
 
 public class SplashScreen extends AppCompatActivity {
@@ -20,6 +24,9 @@ public class SplashScreen extends AppCompatActivity {
         String indicator = getIntent().getStringExtra("indicator");
         avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
         avi.setIndicator(indicator);
+
+        ImageView logo = (ImageView) findViewById(R.id.logosplash);
+        Picasso.with(getApplicationContext()).load(R.drawable.logobukaamal).fit().into(logo);
 
         if (!splashLoaded) {
             new Handler().postDelayed(new Runnable() {
