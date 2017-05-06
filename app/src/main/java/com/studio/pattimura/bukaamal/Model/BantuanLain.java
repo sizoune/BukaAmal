31d4746d6e45e9394e0d4a689a38b56c12dc1e22
wildan <muhammadwildaniskandar@gmail.com>
2017-05-07 00:57:16 +0700

    update beranda

---
+++ app/src/main/java/com/studio/pattimura/bukaamal/Model/BantuanLain.java
@@ -0,0 +1,68 @@
+package com.studio.pattimura.bukaamal.Model;
+
+/**
+ * Created by wildan on 07/05/17.
+ */
+
+public class BantuanLain {
+    private String gambar, tanggal, judul, deskripsi;
+    private int danaterkumpul;
+    private float persen;
+
+    public BantuanLain(String gambar, String tanggal, String judul, String deskripsi, int danaterkumpul, float persen) {
+        this.gambar = gambar;
+        this.tanggal = tanggal;
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.danaterkumpul = danaterkumpul;
+        this.persen = persen;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public int getDanaterkumpul() {
+        return danaterkumpul;
+    }
+
+    public void setDanaterkumpul(int danaterkumpul) {
+        this.danaterkumpul = danaterkumpul;
+    }
+
+    public float getPersen() {
+        return persen;
+    }
+
+    public void setPersen(float persen) {
+        this.persen = persen;
+    }
+}
